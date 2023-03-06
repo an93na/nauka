eb2d23d9e4167ea671faa804da8ab0ef8c817c42
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-06 11:22:41 +0100

    changes

--- dom/index.js
+++ dom/index.js
@@ -4,3 +4,6 @@ let ostatniElementTd = elementyTd[elementyTd.length-1];
 console.log('Bieżący dokument zawiera: '+ elementyTd.length + ' elementów td');
 console.log("Pierwszy element to: ", pierwszyElementTd);
 console.log("Ostatni element to: ", ostatniElementTd);
+
+const pierwszyTd = document.querySelector("td");
+console.log("Pierwszy element td to : ", pierwszyTd);
