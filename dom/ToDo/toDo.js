d79bbf406df2f77f4e1d07579b58267408eb16e6
Anna Giermakowska <an.giermakowska@gmail.com>
2023-03-10 22:29:37 +0100

    changes

--- dom/ToDo/toDo.js
+++ dom/ToDo/toDo.js
@@ -5,6 +5,11 @@ function znajdzElementy(selector){
 function znajdzElement(selektor) {
     return document.querySelector(selektor);
 }
+function zrobElement(nazwa,tekst){
+    const element = document.createElement(nazwa);
+    element.innerText = tekst;
+    return element;
+}
 
 const lista = znajdzElement("ul");
 const dodaj = znajdzElement("button");
@@ -13,4 +18,15 @@ const input = znajdzElement("input[name='rzecz']");
 dodaj.addEventListener('click', () => {
     // console.log('klik');
     const rzecz = input.value;
+    if (input.value !== ""){
+        input.value="";
+            const elLi = zrobElement("li",rzecz);
+            const elUsun = zrobElement("button","usun");
+            lista.appendChild(elLi);
+            elLi.appendChild(elUsun);
+    }
+    else {
+        const ostrzezenie =  zrobElement("div","nie można dodac pustej czynności");
+    }
+    
 })
